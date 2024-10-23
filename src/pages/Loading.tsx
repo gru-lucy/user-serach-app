13fe16391a00592f2ee326bd635b6e6fb52ae7f8
gru-lucy <cbcrypto2017@gmail.com>
2024-10-23 05:17:51 +0300

    autoformat

--- src/pages/Loading.tsx
+++ src/pages/Loading.tsx
@@ -1,13 +1,12 @@
 const Loading = () => {
-    return (
-      <div className="flex items-center justify-center h-screen bg-gray-100">
-        <div className="flex flex-col items-center">
-          <div className="loader border-t-4 border-b-4 border-blue-600 rounded-full w-16 h-16 animate-spin"></div>
-          <p className="mt-4 text-lg text-gray-700">Loading...</p>
-        </div>
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-100">
+      <div className="flex flex-col items-center">
+        <div className="loader border-t-4 border-b-4 border-blue-600 rounded-full w-16 h-16 animate-spin"></div>
+        <p className="mt-4 text-lg text-gray-700">Loading...</p>
       </div>
-    );
-  };
-  
-  export default Loading;
-  
+    </div>
+  );
+};
+
+export default Loading;
