f9c78a1a3642b95e72a38223ab472061d6d63e7e
gru-lucy <cbcrypto2017@gmail.com>
2024-10-29 10:50:58 +0200

    added table component

---
+++ src/components/UsersTable.tsx
@@ -0,0 +1,63 @@
+import { useNavigate } from "react-router-dom";
+import type { User } from "../utils/types/user";
+
+
+interface UsersTableProps {
+  users: User[];
+  isLoading: boolean;
+  isError: boolean;
+  isSuccess: boolean;
+}
+
+export const UsersTable = ({ isLoading, isError, isSuccess, users }: UsersTableProps) => {
+  const navigate = useNavigate()
+
+  return (
+    <table className="min-w-full bg-white border border-gray-200">
+      <thead>
+        <tr>
+          <th className="px-4 py-2 text-left border-b">Name</th>
+          <th className="px-4 py-2 text-left border-b">Email</th>
+          <th className="px-4 py-2 text-left border-b">Phone</th>
+          <th className="px-4 py-2 text-left border-b">Company</th>
+          <th className="px-4 py-2 text-left border-b">Role</th>
+        </tr>
+      </thead>
+      <tbody>
+        {isError && (
+          <tr>
+            <td colSpan={5} className="text-center px-4 py-2">
+              Something went wrong
+            </td>
+          </tr>
+        )}
+        {isLoading && (
+          <tr>
+            <td colSpan={5} className="text-center px-4 py-2">
+              Loading...
+            </td>
+          </tr>
+        )}
+        {isSuccess && users.length === 0 && (
+          <tr>
+            <td colSpan={5} className="text-center px-4 py-2">
+              No users found
+            </td>
+          </tr>
+        )}
+        {isSuccess &&
+          users.length > 0 &&
+          users.map((user) => (
+            <tr key={user.id} className="cursor-pointer hover:bg-gray-200" onClick={() => { navigate(`/users/${user.id}`) }}>
+              <td className="px-4 py-2 border-b h-20">
+                {user.firstName} {user.lastName}
+              </td>
+              <td className="px-4 py-2 border-b h-20">{user.email}</td>
+              <td className="px-4 py-2 border-b h-20">{user.phone}</td>
+              <td className="px-4 py-2 border-b h-20">{user.company.name}</td>
+              <td className="px-4 py-2 border-b h-20">{user.role}</td>
+            </tr>
+          ))}
+      </tbody>
+    </table>)
+}
--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,10 +1,9 @@
 import { useState, useCallback, useMemo, useEffect } from "react";
 import { useGetUsersQuery } from "../../slices/userSlice";
 import useDebounce from "../../hooks/useDebounce";
-import { useNavigate } from "react-router-dom";
+import { UsersTable } from "../../components/UsersTable";
 
 const Users = () => {
-  const navigate = useNavigate()
   const [searchTerm, setSearchTerm] = useState("");
   const [page, setPage] = useState(1);
   const pageSize = 5;
@@ -47,53 +46,7 @@ const Users = () => {
       />
 
       <div className="overflow-x-auto">
-        <table className="min-w-full bg-white border border-gray-200">
-          <thead>
-            <tr>
-              <th className="px-4 py-2 text-left border-b">Name</th>
-              <th className="px-4 py-2 text-left border-b">Email</th>
-              <th className="px-4 py-2 text-left border-b">Phone</th>
-              <th className="px-4 py-2 text-left border-b">Company</th>
-              <th className="px-4 py-2 text-left border-b">Role</th>
-            </tr>
-          </thead>
-          <tbody>
-            {isError && (
-              <tr>
-                <td colSpan={5} className="text-center px-4 py-2">
-                  Something went wrong
-                </td>
-              </tr>
-            )}
-            {isLoading && (
-              <tr>
-                <td colSpan={5} className="text-center px-4 py-2">
-                  Loading...
-                </td>
-              </tr>
-            )}
-            {isSuccess && users.length === 0 && (
-              <tr>
-                <td colSpan={5} className="text-center px-4 py-2">
-                  No users found
-                </td>
-              </tr>
-            )}
-            {isSuccess &&
-              users.length > 0 &&
-              users.map((user) => (
-                <tr key={user.id} className="cursor-pointer hover:bg-gray-200" onClick={() => {navigate(`/users/${user.id}`)}}>
-                  <td className="px-4 py-2 border-b h-20">
-                    {user.firstName} {user.lastName}
-                  </td>
-                  <td className="px-4 py-2 border-b h-20">{user.email}</td>
-                  <td className="px-4 py-2 border-b h-20">{user.phone}</td>
-                  <td className="px-4 py-2 border-b h-20">{user.company.name}</td>
-                  <td className="px-4 py-2 border-b h-20">{user.role}</td>
-                </tr>
-              ))}
-          </tbody>
-        </table>
+        <UsersTable isError={isError} isLoading={isLoading} isSuccess={isSuccess} users={users}></UsersTable>
       </div>
 
       <div className="flex justify-between items-center mt-4">
